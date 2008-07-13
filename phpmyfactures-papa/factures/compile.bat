831ea88296e89e90204342e31b5bf2bc342a0423
samkos <samkos@3f4560be-fcb6-4516-b323-ae570ede3bb7>
2008-07-13 03:36:53 +0000

    compile.bat non suivi par SVN

--- phpmyfactures-papa/factures/compile.bat
+++
@@ -1,7 +0,0 @@
-
-PATH=c:\program Files\MiKTeX 2.5\miktex\bin;C:\WINDOWS\system32;c:\windows;c:\program Files\ghostgum\gsview
-latex "\nonstopmode\input" all.tex
-dvips all
-rem copy all.ps c:\Oyak\facture.ps
-rem copy all.ps c:\Oyak\ToPrint\facture.ps
-rem gsprint all.ps
